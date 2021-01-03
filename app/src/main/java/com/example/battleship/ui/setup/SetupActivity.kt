3ca73dd2adb1b5f89da581b412f66437a9e8abe5
Dmitry Shkadarevich <41323462+KastusKalinovski@users.noreply.github.com>
2021-01-03 20:50:06 +0300

    Added ability to place ships manually

--- app/src/main/java/com/example/battleship/ui/setup/SetupActivity.kt
+++ app/src/main/java/com/example/battleship/ui/setup/SetupActivity.kt
@@ -1,17 +1,22 @@
 package com.example.battleship.ui.setup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.battleship.R
-import com.example.battleship.data.Board
-import com.example.battleship.data.Player
+import com.example.battleship.data.*
+import com.example.battleship.internal.FieldOccupiedException
 import kotlinx.android.synthetic.main.activity_setup.*
 import java.lang.StrictMath.floor
 
 class SetupActivity : AppCompatActivity() {
     private lateinit var board: Board
     private lateinit var boardAdapter: BoardGridAdapter
+    private lateinit var shipAdapter: ShipListAdapter
+
+    private var selectedShip: Ship? = null
+    private lateinit var shipList: ArrayList<Ship>
+    private var shipDirection = Orientation.VERTICAL
 
     private val player = Player("Dmitry", 0)
 
@@ -20,10 +25,29 @@ class SetupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setup)
 
         initBoard()
+        initShips()
 
         randomButton.setOnClickListener {
             generateRandomShips()
+            randomButton.visibility = View.GONE;
+            manualButton.visibility = View.GONE;
+            startButton.visibility = View.VISIBLE;
         }
+
+        manualButton.setOnClickListener {
+            randomButton.visibility = View.GONE;
+            manualButton.visibility = View.GONE;
+            shipsLayout.visibility = View.VISIBLE;
+        }
+
+        rotateButton.setOnClickListener {
+            if (shipDirection == Orientation.VERTICAL)
+                shipDirection = Orientation.HORIZONTAL
+            else
+                shipDirection = Orientation.VERTICAL
+            selectedShip?.orientation = shipDirection
+        }
+
     }
 
     private fun initBoard() {
@@ -35,11 +59,54 @@ class SetupActivity : AppCompatActivity() {
         boardGridView.adapter = boardAdapter
     }
 
+    private fun initShips() {
+        shipList = arrayListOf()
+        shipList.add(Ship(ShipType.CARRIER))
+        shipList.add(Ship(ShipType.CRUISER))
+        shipList.add(Ship(ShipType.DESTROYER))
+        shipList.add(Ship(ShipType.SUBMARINE))
+        shipList.add(Ship(ShipType.BATTLESHIP))
+
+        shipAdapter = ShipListAdapter(this, shipList)
+
+        shipListView.adapter = shipAdapter
+
+        shipListView.setOnItemClickListener { _, view, position, _ ->
+            selectedShip = shipAdapter.getItem(position) as Ship
+            selectedShip?.orientation = shipDirection
+            shipAdapter.selectedPosition = position;
+            shipAdapter.notifyDataSetChanged();
+        }
+    }
+
     private fun handleBoardClick(view: View, position: Int) {
 
         val x: Int = floor((position / board.boardX).toDouble()).toInt()
         val y: Int = position % board.boardX
 
+        if (selectedShip != null) {
+
+            val ship = selectedShip
+
+            try {
+                player.tryPlaceShip(board, ship!!, Coordinate(x, y))
+                boardAdapter.refresh(board.getFieldStatus())
+
+                selectedShip = null
+                shipList.remove(ship)
+                shipAdapter.selectedPosition = -1
+                shipAdapter.notifyDataSetChanged()
+
+            } catch (foe: FieldOccupiedException) {
+
+                ship!!.coords.clear()
+            }
+        }
+        //If all ships placed, start fucking game...
+        if (shipList.isEmpty()){
+            rotateButton.visibility = View.GONE
+            startButton.visibility = View.VISIBLE
+        }
     }
 
     private fun generateRandomShips() {
---
+++ app/src/main/java/com/example/battleship/ui/setup/ShipListAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.battleship.ui.setup
+
+import android.content.Context
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.battleship.R
+import com.example.battleship.data.Ship
+
+class ShipListAdapter(private val context: Context, private val dataSource: ArrayList<Ship>): BaseAdapter(){
+
+    var selectedPosition: Int = -1
+
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        val rowView = inflater.inflate(R.layout.ship_item, parent, false)
+
+        val shipView = rowView.findViewById(R.id.shipView) as TextView
+
+        shipView.text = dataSource[position].shipType.shipName
+
+        if (selectedPosition == position) {
+            shipView.setTypeface(null, Typeface.BOLD)
+        } else {
+            shipView.setTypeface(null, Typeface.NORMAL)
+        }
+
+        return rowView
+    }
+
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+}
