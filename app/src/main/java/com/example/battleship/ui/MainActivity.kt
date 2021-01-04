21c785ccd12d030c3980cff5c2b773272679f831
Dmitry Shkadarevich <41323462+KastusKalinovski@users.noreply.github.com>
2021-01-04 11:03:42 +0300

    Add Login and Logout via AuthUI

--- app/src/main/java/com/example/battleship/ui/MainActivity.kt
+++ app/src/main/java/com/example/battleship/ui/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.battleship.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.battleship.R
+import com.example.battleship.data.firebase.FirebaseSource
+import com.example.battleship.ui.login.LoginActivity
 import com.example.battleship.ui.setup.SetupActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +19,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getStartedButton.setOnClickListener {
+            launchLoginActivity()
+        }
+
+        logoutButton.setOnClickListener {
+
+            AuthUI.getInstance().signOut(this@MainActivity).addOnCompleteListener {
+                Toast.makeText(applicationContext, getString(R.string.user_signed_out), Toast.LENGTH_SHORT).show()
+                launchLoginActivity()
+            }
+        }
+
+        continueButton.setOnClickListener {
             val intent = Intent(this, SetupActivity::class.java)
-            this.startActivity(intent)
-            finish()
+            startActivity(intent)
+        }
+    }
+
+    private fun launchLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        this.startActivity(intent)
+        finish()
+    }
+
+
+    public override fun onStart() {
+        super.onStart()
+        updateUI(FirebaseSource().currentUser())
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+        if (currentUser != null) {
+
+            welcomeView.text = String.format("Welcome - %s", currentUser.displayName)
+            getStartedButton.visibility = View.GONE
+            logoutButton.visibility = View.VISIBLE
+            continueButton.visibility = View.VISIBLE
+
+        } else {
+
+            getStartedButton.visibility = View.VISIBLE
+            logoutButton.visibility = View.GONE
+            continueButton.visibility = View.GONE
         }
     }
 }
